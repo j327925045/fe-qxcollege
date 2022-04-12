f01a701af89d10b2eab1df08f3b81cb0c43b7968
liwenliang <liwenliang@aiforward.com>
2022-04-12 18:57:29 +0800

    feat: 打包地址

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ const resolve = (dir) => path.join(__dirname, dir)
 
 module.exports = {
   // 基本路径
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: '/',
   // 输出文件目录
   outputDir: 'dist',
   // eslint-loader 是否在保存的时候检查
